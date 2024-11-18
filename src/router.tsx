037b0098dd1eee9e9011fc27d0c36a76c4b5b909
lemus <lemus777@gmail.com>
2024-11-18 21:58:16 +0100

    base url import component fix

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from '@/layouts/AppLayout'
 import DashboardView from '@/views/DashboardView'
-import CreateProjectView from './views/projects/CreateProjectView'
+import CreateProjectView from '@/views/projects/CreateProjectView'
 
 export default function Router() {
   return (
