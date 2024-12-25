7c3bab4c54874c63e8ccaca102f44071b75c96fa
lemus <lemus777@gmail.com>
2024-12-25 20:04:03 +0100

    project view and tasks

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -6,13 +6,11 @@ import EditProjectForm from "@/components/projects/EditProjectForm"
 export default function EditProjectView() {
   const params = useParams()
   const projectId = params.projectId!
-
   const { data, isLoading, isError } = useQuery({
     queryKey: ['editProject', projectId],
     queryFn: () => getProjectById(projectId),
     retry: false
   })
-
   if (isLoading) return 'Cargando'
   if (isError) return <Navigate to='/404' />
   if (data) return <EditProjectForm data={data} projectId={projectId} />
