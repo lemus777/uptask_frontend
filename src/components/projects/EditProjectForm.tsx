cc3047da9786b7008c4e75979e304f9b24ab3669
lemus <lemus777@gmail.com>
2024-12-25 19:23:04 +0100

    invalidate queries when we need the data updated

--- src/components/projects/EditProjectForm.tsx
+++ src/components/projects/EditProjectForm.tsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import ProjectForm from "./ProjectForm";
 import { useForm } from "react-hook-form";
 import { Project, ProjectFormData } from "@/Types/index";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateProject } from "@/api/ProjectAPI";
 import { toast } from "react-toastify";
 
@@ -19,12 +19,15 @@ export default function EditProjectForm({data, projectId} : EditProjectFormProps
     description: data.description
   }})
 
+  const queryClient = useQueryClient()
   const { mutate } = useMutation({
     mutationFn: updateProject,
     onError: (error) => {
       toast.error(error.message)
     },
     onSuccess: (data) => {
+      queryClient.invalidateQueries({queryKey: ['projects']})
+      queryClient.invalidateQueries({queryKey: ['editProject', projectId]})
       toast.success(data)
       navigate('/')
     }
