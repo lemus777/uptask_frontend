03287d134ad99dfa8d52ef26b01c5efd72c4a4fe
lemus777 <lemus777@gmail.com>
2025-01-01 19:34:01 +0100

    asigning colors to each status

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -17,6 +17,14 @@ const initialStatusGroups: GroupedTasks = {
   completed: []
 };
 
+const statusStyles : { [key: string] : string} = {
+  pending: 'border-t-slate-500',
+  onHold: 'border-t-pink-500',
+  inProgress: 'border-t-blue-500',
+  underReview: 'border-t-amber-500',
+  completed: 'border-t-emerald-500'
+}
+
 const statusTranslations : { [key: string] : string} = {
   pending: 'Pendiente',
   onHold: 'En espera',
@@ -32,7 +40,6 @@ export default function TaskList({ tasks }: TaskListProps) {
     return { ...acc, [task.status]: currentGroup };
   }, initialStatusGroups);
 
-  console.log(groupedTasks);
   return (
     <>
       <h2 className="text-5xl font-black my-10">Tareas</h2>
@@ -41,7 +48,7 @@ export default function TaskList({ tasks }: TaskListProps) {
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
             <h3
-              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
             >{statusTranslations[status]}</h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
