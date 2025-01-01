4d6de872b70b3e56d36ba8e5818044bf25d8eb6f
lemus777 <lemus777@gmail.com>
2025-01-01 19:24:57 +0100

    showing the state name

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -14,8 +14,16 @@ const initialStatusGroups: GroupedTasks = {
   onHold: [],
   inProgress: [],
   underReview: [],
-  completed: [],
+  completed: []
 };
+
+const statusTranslations : { [key: string] : string} = {
+  pending: 'Pendiente',
+  onHold: 'En espera',
+  inProgress: 'En progreso',
+  underReview: 'En revisión',
+  completed: 'Completado'
+}
 
 export default function TaskList({ tasks }: TaskListProps) {
   const groupedTasks = tasks.reduce((acc, task) => {
@@ -32,6 +40,9 @@ export default function TaskList({ tasks }: TaskListProps) {
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+            <h3
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}
+            >{statusTranslations[status]}</h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">
