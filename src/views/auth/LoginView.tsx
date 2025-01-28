b0ba7ace9aed85ff1addaf833dab921671d90df1
lemus <lemus777@gmail.com>
2025-01-28 11:27:55 +0100

    reset password view

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,42 +1,48 @@
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query";
 import { UserLoginForm } from "@/Types/index";
 import ErrorMessage from "@/components/ErrorMessage";
 import { authenticateUser } from "@/api/AuthAPI";
 import { toast } from "react-toastify";
 
 export default function LoginView() {
-
   const initialValues: UserLoginForm = {
-    email: '',
-    password: '',
-  }
-  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+    email: "",
+    password: "",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
 
   const { mutate } = useMutation({
     mutationFn: authenticateUser,
     onError: (error) => {
-      toast.error(error.message)
+      toast.error(error.message);
     },
     onSuccess: (data) => {
-      toast.success(data)
-    }
-  })
+      toast.success(data);
+    },
+  });
 
-  const handleLogin = (formData: UserLoginForm) => mutate(formData)
+  const handleLogin = (formData: UserLoginForm) => mutate(formData);
 
   return (
     <>
+      <h1 className="text-5xl font-black text-white">Iniciar sesión</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Comienza a planear tus proyectos {""}
+        <span className=" text-fuchsia-500 font-bold"> iniciando sesión en este formulario</span>
+      </p>
       <form
         onSubmit={handleSubmit(handleLogin)}
-        className="space-y-8 p-10 bg-white"
+        className="space-y-8 p-10 mt-10 bg-white"
         noValidate
       >
         <div className="flex flex-col gap-5">
-          <label
-            className="font-normal text-2xl"
-          >Email</label>
+          <label className="font-normal text-2xl">Email</label>
 
           <input
             id="email"
@@ -51,15 +57,11 @@ export default function LoginView() {
               },
             })}
           />
-          {errors.email && (
-            <ErrorMessage>{errors.email.message}</ErrorMessage>
-          )}
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
 
         <div className="flex flex-col gap-5">
-          <label
-            className="font-normal text-2xl"
-          >Password</label>
+          <label className="font-normal text-2xl">Password</label>
 
           <input
             type="password"
@@ -76,16 +78,25 @@ export default function LoginView() {
 
         <input
           type="submit"
-          value='Iniciar Sesión'
+          value="Iniciar Sesión"
           className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
         />
       </form>
       <nav className="mt-10 flex flex-col space-y-4">
-          <Link
-            to={'/auth/register'}
-            className="text-center text-gray-300 font-normal"
-          >¿No tienes cuenta? Crear una</Link>
+        <Link
+          to={"/auth/register"}
+          className="text-center text-gray-300 font-normal"
+        >
+          ¿No tienes cuenta? Crear una
+        </Link>
+
+        <Link
+          to={"/auth/forgot-password"}
+          className="text-center text-gray-300 font-normal"
+        >
+          ¿Olvidaste tu contraseña? Restablecer
+        </Link>
       </nav>
     </>
-  )
+  );
 }
