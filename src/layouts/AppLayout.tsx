6ff2a4ea9da6e8c2f168bb8613ec76befb8f715e
lemus <lemus777@gmail.com>
2025-02-13 12:18:40 +0100

    defining user Schema

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -13,7 +13,7 @@ export default function AppLayout() {
     return <Navigate to='/auth/login' />
   }
 
-  return (
+  if(data) return (
     <>
       <header className="bg-gray-800 py-5">
         <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
