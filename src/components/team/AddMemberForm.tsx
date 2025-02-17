dcd5124da811e0e9db919b86bd6145864c0b8f5f
lemus <lemus777@gmail.com>
2025-02-17 11:30:07 +0100

    creating a mutation to find users

--- src/components/team/AddMemberForm.tsx
+++ src/components/team/AddMemberForm.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import ErrorMessage from "../ErrorMessage";
 import { TeamMemberForm } from "types";
+import { findUserByEmail } from "@/api/TeamAPI";
 
 export default function AddMemberForm() {
   const initialValues: TeamMemberForm = {
@@ -18,9 +19,14 @@ export default function AddMemberForm() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const mutation = useMutation({});
+  const mutation = useMutation({
+    mutationFn: findUserByEmail
+  });
 
-  const handleSearchUser = async () => {};
+  const handleSearchUser = async (formData: TeamMemberForm) => {
+    const data = {projectId, formData}
+    mutation.mutate(data)
+  };
 
   return (
     <>
@@ -55,6 +61,10 @@ export default function AddMemberForm() {
           value="Buscar Usuario"
         />
       </form>
+      <div className="mt-10">
+        {mutation.isPending && <p className="text-center">Cargando...</p>}
+        {mutation.error && <p className="text-center">{mutation.error.message}</p>}
+      </div>
     </>
   );
 }
