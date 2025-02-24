880a06ed9d68819e92439877911652240d838989
lemus <lemus777@gmail.com>
2025-02-24 12:07:42 +0100

    creating history changes

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -25,6 +25,7 @@ export async function getTaskById({projectId, taskId} : Pick<TaskAPI, 'projectId
   try {
     const url = `/projects/${projectId}/tasks/${taskId}`
     const { data } = await api(url)
+    return data
     const response = taskSchema.safeParse(data)
     if(response.success) {
       return response.data
