98768f4661879e1676b50a09283929c05635f9fa
lemus <lemus777@gmail.com>
2025-03-05 13:13:24 +0100

    invalidating queries after create a note

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,7 +1,7 @@
 import { NoteFormData } from '@/types/index'
 import { useForm } from 'react-hook-form'
 import ErrorMessage from '../ErrorMessage'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createNote } from '@/api/NoteAPI'
 import { toast } from 'react-toastify'
 import { useLocation, useParams } from 'react-router-dom'
@@ -21,6 +21,7 @@ export default function AddNoteForm() {
 
   const { register, handleSubmit, reset, formState: {errors} } =  useForm({defaultValues: initialValues})
 
+  const queryClient = useQueryClient()
   const { mutate } = useMutation({
     mutationFn: createNote,
     onError: (error) => {
@@ -28,6 +29,7 @@ export default function AddNoteForm() {
     },
     onSuccess: (data) => {
       toast.success(data)
+      queryClient.invalidateQueries({queryKey: ["task", taskId]})
     }
   })
   const handleAddNote = (formData: NoteFormData) => {
--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,6 +1,22 @@
+import { Note } from "@/types/index";
+import { formatDate } from "@/utils/utils";
 
-export default function NoteDetail() {
+type NoteDetailProps = {
+  note: Note;
+};
+
+export default function NoteDetail({ note }: NoteDetailProps) {
   return (
-    <div>NoteDetail</div>
-  )
+    <div className="p-3 flex justify-between items-center">
+      <div>
+        <p>
+          {note.content} por:{" "}
+          <span className="font-bold">{note.createdBy.name}</span>
+        </p>
+        <p className="text-xs text-slate-500">
+          {formatDate(note.createdAt)}
+        </p>
+      </div>
+    </div>
+  );
 }
