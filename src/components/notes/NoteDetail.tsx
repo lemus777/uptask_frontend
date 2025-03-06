4050f8b84ae73ffe5da515d340e7b63e37f91fbc
lemus <lemus777@gmail.com>
2025-03-06 12:29:31 +0100

    deleting notes and invalidating queries

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,7 +1,11 @@
+import { deleteNote } from "@/api/NoteAPI";
 import { useAuth } from "@/hooks/useAuth";
 import { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useMemo } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type NoteDetailProps = {
   note: Note;
@@ -10,6 +14,22 @@ type NoteDetailProps = {
 export default function NoteDetail({ note }: NoteDetailProps) {
   const { data, isLoading } = useAuth();
   const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+  const params = useParams()
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const projectId = params.projectId!
+  const taskId = queryParams.get("viewTask")!;
+
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({queryKey: ["task", taskId]})
+    }
+  })
 
   if (isLoading) return "Cargando...";
 
@@ -26,6 +46,7 @@ export default function NoteDetail({ note }: NoteDetailProps) {
         <button
           type="button"
           className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+          onClick={() => mutate({projectId, taskId, noteId: note._id})}
         >
           Eliminar
         </button>
