35edc0c3cece1f697f9488b164bc46e3bcba7e73
lemus <lemus777@gmail.com>
2025-03-11 11:45:58 +0100

    multiple mutations on react query

--- src/components/projects/DeleteProjectModal.tsx
+++ src/components/projects/DeleteProjectModal.tsx
@@ -3,9 +3,13 @@ import { Dialog, Transition } from "@headlessui/react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ErrorMessage from "../ErrorMessage";
+import { CheckPasswordForm } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { checkPassword } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
 
 export default function DeleteProjectModal() {
-  const initialValues = {
+  const initialValues : CheckPasswordForm = {
     password: "",
   };
   const location = useLocation();
@@ -21,7 +25,16 @@ export default function DeleteProjectModal() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleForm = async (formData) => {};
+  const checkUserPasswordMutation = useMutation({
+    mutationFn: checkPassword,
+    onError: (error) => toast.error(error.message)
+  })
+
+  const handleForm = async (formData : CheckPasswordForm) => {
+    await checkUserPasswordMutation.mutateAsync(formData)
+    await checkUserPasswordMutation.mutateAsync(formData)
+    console.log('Después de la mutación')
+  };
 
   return (
     <Transition appear show={show} as={Fragment}>
