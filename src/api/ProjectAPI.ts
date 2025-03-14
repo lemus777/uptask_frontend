930b9e39cb473ebb80a915d6adec2c9577852595
lemus <lemus777@gmail.com>
2025-03-14 13:46:30 +0100

    solving last details

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { dashboardProjectSchema, editProjectSchema, Project, ProjectFormData } from "../types";
+import { dashboardProjectSchema, editProjectSchema, Project, ProjectFormData, projectSchema } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
@@ -31,6 +31,20 @@ export async function getProjectById(id: Project['_id']) {
   try {
     const { data } = await api(`/projects/${id}`)
     const response = editProjectSchema.safeParse(data)
+    if(response.success) {
+      return response.data
+    }
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function getFullProject(id: Project['_id']) {
+  try {
+    const { data } = await api(`/projects/${id}`)
+    const response = projectSchema.safeParse(data)
     if(response.success) {
       return response.data
     }
